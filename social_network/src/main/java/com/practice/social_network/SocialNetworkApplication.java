b092cb24b7388c3daac4959d271d0069b8054dce
Maksym Diachuk <dia4uk.maksim@gmail.com>
2023-05-15 22:32:50 +0300

    Prepared project for deploying on AWS EC2 instance

--- social_network/src/main/java/com/practice/social_network/SocialNetworkApplication.java
+++ social_network/src/main/java/com/practice/social_network/SocialNetworkApplication.java
@@ -1,7 +1,10 @@
 package com.practice.social_network;
 
+import ch.qos.logback.classic.util.ContextInitializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -9,9 +12,12 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
         org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class)
 @EnableJpaRepositories
 @EnableJpaAuditing
-public class SocialNetworkApplication {
+@PropertySource("classpath:/local_db_creds.properties")
+public class SocialNetworkApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
+        System.out.println("Init");
         SpringApplication.run(SocialNetworkApplication.class, args);
+
     }
 
 }
